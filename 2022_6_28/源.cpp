8bc6e4dfa35b169f267941355067d21d1252bebc
kurayamasy <102274637+kurayamasy@users.noreply.github.com>
2022-06-28 20:36:37 +0800

    Add files via upload

---
+++ 2022_6_28/源.cpp
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<cmath>
+//#include<string.h>
+//void print(int* p,int sz)
+//{
+//	int i = 0;
+//	for (i; i < sz; i++)
+//	{
+//		printf("%d ", (*p++));
+//
+//	}
+//
+//}
+//int main()
+//void invert(int* p, int sz)
+//{
+//	int i = 0;
+//	while (sz != i)
+//	{
+//		int t = 0;
+//		t = *(p + i);
+//		*(p + i) = *(p + sz-1);
+//		*(p + sz-1) = t;
+//		 i++;
+//		 sz--;
+//
+//
+//	}
+//
+//}
+//int main()
+//{
+//	int arr1[8] = { 1,2,3,4,5,6,7,8 };
+//	invert(arr1, 8);
+//	int i = 0;
+//		for (i; i < 8; i++)
+//		{
+//			printf("%d ", arr1[i]);
+//	     }
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[7] = { " " };
+//	int i = 1;
+//	for (i; i < 6; i++)
+//	{
+//		int j = i;
+//
+//
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int count = 0;
+//	int n = 20;
+//	while (n)
+//	{
+//		n -= 1;
+//		if ((20 - n)%2 == 0)
+//		{
+//			count += 1;
+//		}
+//	
+//		count++;
+//
+//	}
+//	printf("%d", count);
+//
+//	return 0;
+//}
