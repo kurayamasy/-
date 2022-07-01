3a23811efe66ee24df58a77bf55dd4e18db3294e
kurayamasy <102274637+kurayamasy@users.noreply.github.com>
2022-07-01 12:23:05 +0800

    Add files via upload

---
+++ 2022_6_29/源.cpp
@@ -0,0 +1,104 @@
+#include <stdio.h>
+//int main()
+//{
+//    int i = 0;
+//    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//    for (i = 0; i <= 12; i++)
+//    {
+//  
+//        printf("hello bit\n");
+//    }
+//    return 0;
+//}
+\
+//int my_strlen(const char* arr)
+//{
+//	int count = 0;
+//	while (*arr != '\0')
+//	{
+//		arr++;
+//		count++;
+//
+//
+//	}
+//
+//	return count;
+//}
+//
+//
+//
+//int main()
+//{
+//	char arr1[] = { "hello bit" };
+//	int a = my_strlen(arr1);
+//	printf("%d",a);
+//
+//
+//	return 0;
+//}
+//void my_strcpy(char* dest, const char* sour)
+//{
+//	while (*sour != '\0')
+//	{
+//		*dest++ = *sour++;
+//	
+//	}
+//
+//}
+//int main()
+//{
+//	char arr1[100] = { "0" };
+//	char arr2[] = { "i love bite" };
+//	my_strcpy(arr1, arr2);
+//	int i = 0;
+//	for (i; i < 12; i++)
+//	{
+//		printf("%c", arr1[i]);
+//		
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int m, n = 0;
+//	scanf_s("%d %d\n", &n, &m);
+//	int i = 0;
+//	int arr1[n] = { 0 };
+//	for (i; i < n; i++)
+//	{
+//		
+//		scanf_s("%d ", arr1[i]);
+//	}
+//	printf("%d", arr1);
+//
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int t = 1;
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	while (n % t)
+//	{
+//		int i = n / t;
+//		int j = n % t;
+//
+//		if ((n / t) % 2 == 1)
+//		{
+//			
+//			n = ((i / 10) * 10 + 1) * t + j * t;
+//		}
+//		else
+//		{
+//			
+//			n = ((i / 10) * 10) * t + j * t;
+//
+//		}
+//		t *= 10;
+//	}
+//
+//	printf("%d", n);
+//	  return 0;
+//}
