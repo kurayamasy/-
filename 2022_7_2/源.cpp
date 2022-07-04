6f55515bee3c9f2092e556a25e66c43a5cbfc000
kurayamasy <102274637+kurayamasy@users.noreply.github.com>
2022-07-04 17:14:05 +0800

    Add files via upload

---
+++ 2022_7_2/源.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	int n = 0;
+//	int count = 0;
+//	scanf("%d", &n);
+//		while (n)
+//		{
+//			n
+//
+//
+//	}
+//
+//}
+//
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,0 };
+	int(*p)[10] = &arr;
+	return 0;
+}
